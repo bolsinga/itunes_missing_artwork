99891fe7eb780e24ba40118766ccac4c6e30340e
Greg Bolsinga <bolsinga@mac.com>
2021-03-28 18:55:42 -0700

    Add Argument Parser and CupertinoJWT to tool dependencies. log token before logging missing artwork.

--- Package.swift
+++ Package.swift
@@ -8,11 +8,18 @@ let package = Package(
     platforms: [
         .macOS(.v11),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.1"),
+        .package(url: "https://github.com/terwanerik/CupertinoJWT", from: "0.2.2"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "itunes_json",
-            dependencies: []),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"),
+                           .product(name: "CupertinoJWT", package: "CupertinoJWT"),
+            ]
+        ),
     ]
 )
--- Sources/itunes_json/main.swift
+++ Sources/itunes_json/main.swift
@@ -1,10 +1,37 @@
 import Foundation
+import CupertinoJWT
+import ArgumentParser
 
-do {
-    let missingMediaArtworks = try MissingArtwork.gatherMissingArtwork()
-    print("\(missingMediaArtworks.count) Missing Artworks:")
-    missingMediaArtworks.sorted().forEach { print("\($0.searchQueryRepresentation)") }
-} catch {
-    print("unable to get missing art")
-    exit(1)
+struct Generate : ParsableCommand {
+    struct SigningData : ExpressibleByArgument {
+        var p8 : String
+
+        init?(argument: String) {
+            do {
+                p8 = try String(contentsOf: URL(fileURLWithPath: argument))
+            } catch {
+                return nil
+            }
+        }
+    }
+
+    @Option(help: "The keyID for the JWT")
+    var keyID: String
+
+    @Option(help: "The teamID for the JWT")
+    var teamID: String
+
+    @Argument(help: "The path to the p8 file")
+    var signingData: SigningData
+
+    func run() throws {
+        let token = try JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60).sign(with: signingData.p8)
+        print("\(token)")
+
+        let missingMediaArtworks = try MissingArtwork.gatherMissingArtwork()
+        print("\(missingMediaArtworks.count) Missing Artworks:")
+        missingMediaArtworks.sorted().forEach { print("\($0.searchQueryRepresentation)") }
+    }
 }
+
+Generate.main()
