714c98e1ac3e9f29c67ddabcabad7e4d1e367fee
Greg Bolsinga <bolsinga@mac.com>
2021-10-30 20:05:20 -0700

    Update to macos 12 and Swift 5.5 (#13)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "itunes_missing_artwork",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4"),
