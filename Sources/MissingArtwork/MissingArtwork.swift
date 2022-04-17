2a626a4bcddf959a72075cf72a6341c872c7f38d
Greg Bolsinga <bolsinga@mac.com>
2022-04-17 11:37:36 -0700

    Lint files with swift-format 5.6

--- Sources/MissingArtwork/MissingArtwork.swift
+++ Sources/MissingArtwork/MissingArtwork.swift
@@ -8,46 +8,47 @@
 import Foundation
 import iTunesLibrary
 
-public enum MissingArtwork : Hashable, Comparable {
-    case ArtistAlbum(String, String)
-    case CompilationAlbum(String)
+public enum MissingArtwork: Hashable, Comparable {
+  case ArtistAlbum(String, String)
+  case CompilationAlbum(String)
 }
 
-extension MissingArtwork : CustomStringConvertible {
-    public var description : String {
-        switch self {
-        case let .ArtistAlbum(artist, album):
-            return "\(artist): \(album)"
-        case let .CompilationAlbum(title):
-            return "\(title)"
-        }
+extension MissingArtwork: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case let .ArtistAlbum(artist, album):
+      return "\(artist): \(album)"
+    case let .CompilationAlbum(title):
+      return "\(title)"
     }
+  }
 }
 
 extension MissingArtwork {
-    var simpleRepresentation : String {
-        switch self {
-        case let .ArtistAlbum(artist, album):
-            return "\(artist) \(album)"
-        case let .CompilationAlbum(title):
-            return title
-        }
+  var simpleRepresentation: String {
+    switch self {
+    case let .ArtistAlbum(artist, album):
+      return "\(artist) \(album)"
+    case let .CompilationAlbum(title):
+      return title
     }
+  }
 
-    var fileNameRepresentation : String {
-        self.simpleRepresentation.replacingOccurrences(of: " ", with: "_")
-    }
+  var fileNameRepresentation: String {
+    self.simpleRepresentation.replacingOccurrences(of: " ", with: "_")
+  }
 
-    public static func gatherMissingArtwork() throws -> [MissingArtwork] {
-        let itunes = try ITLibrary(apiVersion: "1.1")
+  public static func gatherMissingArtwork() throws -> [MissingArtwork] {
+    let itunes = try ITLibrary(apiVersion: "1.1")
 
-        return itunes.allMediaItems
-            .filter { $0.mediaKind != .kindBook }
-            .filter { $0.mediaKind != .kindVoiceMemo }
-            .filter { !$0.hasArtworkAvailable || $0.artwork == nil }
-            .compactMap {
-                $0.album.isCompilation ? .CompilationAlbum($0.album.title!)
-                    : .ArtistAlbum($0.artist?.name ?? $0.album.albumArtist!, $0.album.title ?? $0.title)
-            }
-    }
+    return itunes.allMediaItems
+      .filter { $0.mediaKind != .kindBook }
+      .filter { $0.mediaKind != .kindVoiceMemo }
+      .filter { !$0.hasArtworkAvailable || $0.artwork == nil }
+      .compactMap {
+        $0.album.isCompilation
+          ? .CompilationAlbum($0.album.title!)
+          : .ArtistAlbum($0.artist?.name ?? $0.album.albumArtist!, $0.album.title ?? $0.title)
+      }
+  }
 }
--- Sources/MissingArtworkTool/Program.swift
+++ Sources/MissingArtworkTool/Program.swift
@@ -1,45 +1,46 @@
-import Foundation
-import CupertinoJWT
 import ArgumentParser
+import CupertinoJWT
+import Foundation
 import MissingArtwork
 
 @main
-struct Program : AsyncParsableCommand {
-    struct SigningData : ExpressibleByArgument {
-        var p8 : String
+struct Program: AsyncParsableCommand {
+  struct SigningData: ExpressibleByArgument {
+    var p8: String
 
-        init?(argument: String) {
-            do {
-                p8 = try String(contentsOf: URL(fileURLWithPath: argument))
-            } catch {
-                return nil
-            }
-        }
+    init?(argument: String) {
+      do {
+        p8 = try String(contentsOf: URL(fileURLWithPath: argument))
+      } catch {
+        return nil
+      }
     }
+  }
 
-    @Option(help: "The keyID for the JWT")
-    var keyID: String
+  @Option(help: "The keyID for the JWT")
+  var keyID: String
 
-    @Option(help: "The teamID for the JWT")
-    var teamID: String
+  @Option(help: "The teamID for the JWT")
+  var teamID: String
 
-    @Argument(help: "The path to the p8 file")
-    var signingData: SigningData
+  @Argument(help: "The path to the p8 file")
+  var signingData: SigningData
 
-    mutating func run() async throws {
-        let token = try JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60).sign(with: signingData.p8)
+  mutating func run() async throws {
+    let token = try JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60)
+      .sign(with: signingData.p8)
 
-        let allMissingMediaArtworks = try MissingArtwork.gatherMissingArtwork()
-        let missingMediaArtworks = Array(Set<MissingArtwork>(allMissingMediaArtworks))
-        print("\(missingMediaArtworks.count) Missing Artworks")
+    let allMissingMediaArtworks = try MissingArtwork.gatherMissingArtwork()
+    let missingMediaArtworks = Array(Set<MissingArtwork>(allMissingMediaArtworks))
+    print("\(missingMediaArtworks.count) Missing Artworks")
 
-        let sessionConfiguration = URLSessionConfiguration.default;
-        sessionConfiguration.httpAdditionalHeaders = [ "Authorization" : "Bearer \(token)"]
+    let sessionConfiguration = URLSessionConfiguration.default
+    sessionConfiguration.httpAdditionalHeaders = ["Authorization": "Bearer \(token)"]
 
-        let session = URLSession(configuration: sessionConfiguration)
-        
-        let artworkURLFetcher = ArtworkURLFetcher(session)
-        let missingMediaURLs = await artworkURLFetcher.fetch(missingMediaArtworks)
-        print("\(missingMediaURLs)")
-    }
+    let session = URLSession(configuration: sessionConfiguration)
+
+    let artworkURLFetcher = ArtworkURLFetcher(session)
+    let missingMediaURLs = await artworkURLFetcher.fetch(missingMediaArtworks)
+    print("\(missingMediaURLs)")
+  }
 }
