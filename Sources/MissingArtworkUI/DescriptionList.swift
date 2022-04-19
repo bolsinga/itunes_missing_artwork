4a3548e3a1a536363f48463df554c1c948daf5c3
Greg Bolsinga <bolsinga@mac.com>
2022-04-19 14:56:38 -0700

    Add DescriptionList UI
    - Requires making MissingArtwork enum Identifiable.

---
+++ Sources/MissingArtworkUI/DescriptionList.swift
@@ -0,0 +1,36 @@
+//
+//  DescriptionList.swift
+//
+//
+//  Created by Greg Bolsinga on 4/19/22.
+//
+
+import MissingArtwork
+import SwiftUI
+
+public struct DescriptionList: View {
+  public init(missingArtworks: [MissingArtwork]) {
+    self.missingArtworks = missingArtworks
+  }
+
+  let missingArtworks: [MissingArtwork]
+
+  public var body: some View {
+    List {
+      ForEach(missingArtworks) { missingArtwork in
+        Description(missingArtwork: missingArtwork)
+      }
+    }
+  }
+}
+
+struct DescriptionList_Previews: PreviewProvider {
+  static var previews: some View {
+    let missingArtworks = [
+      MissingArtwork.ArtistAlbum("The Stooges", "Fun House"),
+      .CompilationAlbum("Beleza Tropical: Brazil Classics 1"),
+    ]
+
+    DescriptionList(missingArtworks: missingArtworks)
+  }
+}
