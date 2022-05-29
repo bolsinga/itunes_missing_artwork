cc835c3f41e56fe4a51e4c33ce0cec1960c0b4fb
Greg Bolsinga <bolsinga@mac.com>
2022-05-28 18:09:50 -0700

    Add encapsulating MissingArtworkView

---
+++ Sources/MissingArtwork/MissingArtworkView.swift
@@ -0,0 +1,26 @@
+//
+//  MissingArtworkView.swift
+//
+//
+//  Created by Greg Bolsinga on 5/28/22.
+//
+
+import SwiftUI
+
+public struct MissingArtworkView: View {
+  let token: String
+
+  public init(token: String) {
+    self.token = token
+  }
+
+  public var body: some View {
+    DescriptionList(token: token)
+  }
+}
+
+struct MissingArtworkView_Previews: PreviewProvider {
+  static var previews: some View {
+    MissingArtworkView(token: "")
+  }
+}
