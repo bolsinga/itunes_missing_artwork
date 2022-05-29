ca88ac61288b05104a1a2a783f4560f390d9a808
Greg Bolsinga <bolsinga@mac.com>
2022-05-29 08:45:02 -0700

    Move Model into Enclosing View (#72)
    - It will handle the .task action as well.

--- Sources/MissingArtwork/MissingArtworkView.swift
+++ Sources/MissingArtwork/MissingArtworkView.swift
@@ -10,17 +10,23 @@ import SwiftUI
 public struct MissingArtworkView: View {
   let token: String
 
+  @EnvironmentObject var model: Model
+
   public init(token: String) {
     self.token = token
   }
 
   public var body: some View {
     DescriptionList(token: token)
+      .task {
+        await model.fetchMissingArtworks(token: token)
+      }
   }
 }
 
 struct MissingArtworkView_Previews: PreviewProvider {
   static var previews: some View {
     MissingArtworkView(token: "")
+      .environmentObject(Model.preview)
   }
 }
