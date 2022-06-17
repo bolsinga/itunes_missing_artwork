fcc78b49feb7031dd4d4a2004702331ba9063cc5
Greg Bolsinga <bolsinga@mac.com>
2022-06-16 21:18:29 -0700

    No longer need JWT nor MusicAPI code (#94)
    - Rip out the old.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # itunes_missing_artwork
-This uses the [iTunesLibrary framework](https://developer.apple.com/documentation/ituneslibrary) to learn what media do not have artwork. It then uses the [Apple Music API](https://developer.apple.com/documentation/applemusicapi/search) to search for the artwork.
+This uses the [iTunesLibrary framework](https://developer.apple.com/documentation/ituneslibrary) to learn what media do not have artwork. It then uses the [MusicKit](https://developer.apple.com/documentation/MusicKit/) to search for the artwork on Apple's Catalog.
 
 In Catalina, the Music.app has lost 100s of the artwork it used to have for years. The application itself cannot find this artwork. However using the API provided by Apple, much of the artwork can be found. 
-
-This requires an Apple Developer ID and Music API Key. Pass them in as parameters to the command line program. It will emit the URLs for the missing artwork it is able to find at Apple.
--- Sources/MissingArtworkTool/Program.swift
+++ Sources/MissingArtworkTool/Program.swift
@@ -1,38 +1,13 @@
 import ArgumentParser
-import CupertinoJWT
 import Foundation
 import MissingArtwork
 
 @main
 struct Program: AsyncParsableCommand {
-  struct SigningData: ExpressibleByArgument {
-    var p8: String
-
-    init?(argument: String) {
-      do {
-        p8 = try String(contentsOf: URL(fileURLWithPath: argument))
-      } catch {
-        return nil
-      }
-    }
-  }
-
-  @Option(help: "The keyID for the JWT")
-  var keyID: String
-
-  @Option(help: "The teamID for the JWT")
-  var teamID: String
-
-  @Argument(help: "The path to the p8 file")
-  var signingData: SigningData
-
   @MainActor
   mutating func run() async throws {
-    let token = try JWT(keyID: keyID, teamID: teamID, issueDate: Date(), expireDuration: 60 * 60)
-      .sign(with: signingData.p8)
-
     let model = Model()
-    try await model.fetchMissingArtworks(token: token)
+    try await model.fetchMissingArtworks()
     let missingMediaArtworks = model.missingArtworks
     print("\(missingMediaArtworks.count) Missing Artworks")
 
