4dba1a4d920300acb51342e0ada19967f206e2fc
Greg Bolsinga <bolsinga@mac.com>
2022-06-18 14:37:22 -0700

    Track MissingArtworks as @State instead of @Published in the Model
    Simplifying more.

--- Sources/MissingArtwork/Model.swift
+++ Sources/MissingArtwork/Model.swift
@@ -10,23 +10,10 @@ import MusicKit
 
 @MainActor
 public class Model: ObservableObject {
-  @Published public var missingArtworks: [MissingArtwork]
 
-  /// Used for previews.
-  init(missingArtworks: [MissingArtwork]) {
-    self.missingArtworks = missingArtworks
-  }
-
-  public convenience init() {
-    self.init(missingArtworks: [])
-  }
-
-  public func fetchMissingArtworks() async throws {
-    if self.missingArtworks.isEmpty {
-      async let missingArtworks = try MissingArtwork.gatherMissingArtwork()
-
-      self.missingArtworks = try await Array(Set<MissingArtwork>(missingArtworks))
-    }
+  public func fetchMissingArtworks() async throws -> [MissingArtwork] {
+    async let missingArtworks = try MissingArtwork.gatherMissingArtwork()
+    return try await Array(Set<MissingArtwork>(missingArtworks))
   }
 
   func fetchArtworks(missingArtwork: MissingArtwork, term: String) async throws -> [URL] {
