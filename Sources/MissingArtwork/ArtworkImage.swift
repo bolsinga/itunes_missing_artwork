4ce21ecc1123a794550ada1cf5799cfad196204b
Greg Bolsinga <bolsinga@mac.com>
2023-01-14 18:54:11 -0800

    Encapsulate loading into ArtworkImage (#178)

--- Sources/MissingArtwork/ArtworkImage.swift
+++ Sources/MissingArtwork/ArtworkImage.swift
@@ -6,7 +6,24 @@
 //
 
 import AppKit
+import Foundation
 import MusicKit
+
+private enum NoImageError: Error {
+  case noURL(Artwork)
+  case noImage(Artwork)
+}
+
+extension NoImageError: LocalizedError {
+  fileprivate var errorDescription: String? {
+    switch self {
+    case .noURL(let artwork):
+      return "No Image URL Available: \(artwork.description)."
+    case .noImage(let artwork):
+      return "No Image Found: \(artwork.description)."
+    }
+  }
+}
 
 struct ArtworkImage: Equatable {
   static func == (lhs: ArtworkImage, rhs: ArtworkImage) -> Bool {
@@ -29,4 +46,28 @@ struct ArtworkImage: Equatable {
 
   let artwork: Artwork
   var loadingState: LoadingState<NSImage>
+
+  mutating func load() async {
+    guard case .idle = loadingState else {
+      return
+    }
+
+    loadingState = .loading
+
+    do {
+      guard let url = artwork.url(width: artwork.maximumWidth, height: artwork.maximumHeight)
+      else { throw NoImageError.noURL(artwork) }
+
+      let (data, _) = try await URLSession.shared.data(from: url)
+      let nsImage = NSImage.init(data: data)
+
+      if let nsImage {
+        loadingState = .loaded(nsImage)
+      } else {
+        throw NoImageError.noImage(artwork)
+      }
+    } catch {
+      loadingState = .error(error)
+    }
+  }
 }
