76f1a82435dff2d1e1fb2c089477c461d4035fe6
Greg Bolsinga <bolsinga@mac.com>
2023-01-20 19:25:12 -0800

    Encapsulate loading NSImage on an extension to LoadingState<NSImage> (#193)

--- Sources/MissingArtwork/MissingArtworkImage.swift
+++ Sources/MissingArtwork/MissingArtworkImage.swift
@@ -43,7 +43,7 @@ struct MissingArtworkImage: View {
     }
     .frame(width: width)
     .task {
-      await artworkImage.load()
+      await artworkImage.load(artwork: artworkImage.artwork)
     }
   }
 }
