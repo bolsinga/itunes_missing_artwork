06a55bd3686485a580500bebddcab6edbe504831
Greg Bolsinga <bolsinga@mac.com>
2024-11-07 09:39:59 -0800

    add previews with mock data for MissingArtworkImage (#349)

--- Sources/MissingArtwork/MissingArtworkImage.swift
+++ Sources/MissingArtwork/MissingArtworkImage.swift
@@ -8,19 +8,28 @@
 import MusicKit
 import SwiftUI
 
-struct MissingArtworkImage: View {
+// Created so Previews may use fake Artwork.
+protocol MissingArtworkProtocol: Sendable {
+  var backgroundColor: CGColor? { get }
+  var maximumHeight: Int { get }
+}
+
+extension Artwork: MissingArtworkProtocol {}
+
+struct MissingArtworkImage<C: MissingArtworkProtocol>: View {
   let width: CGFloat
 
-  let artwork: Artwork
-  var loadingState: ArtworkPlatformImageModel
+  let artwork: C
+  var loadingState: LoadingModel<PlatformImage, C>
 
   var body: some View {
     Group {
       if loadingState.isIdleOrLoading {
-        if let backgroundColor = artwork.backgroundColor {
-          Color(cgColor: backgroundColor)
-            .frame(width: width, height: CGFloat(artwork.maximumHeight))
-        } else {
+        ZStack {
+          if let backgroundColor = artwork.backgroundColor {
+            Color(cgColor: backgroundColor)
+              .frame(width: width, height: CGFloat(artwork.maximumHeight))
+          }
           ProgressView()
         }
       } else if let error = loadingState.error {
@@ -33,8 +42,48 @@ struct MissingArtworkImage: View {
       }
     }
     .frame(width: width)
-    .task {
-      await loadingState.load(artwork)
-    }
+    .task { await loadingState.load(artwork) }
   }
+}
+
+private struct PreviewArtwork: MissingArtworkProtocol {
+  var backgroundColor: CGColor?
+  var maximumHeight: Int { 300 }
+
+  internal init(backgroundColor: CGColor? = CGColor(red: 0.5, green: 0.5, blue: 1.0, alpha: 1.0)) {
+    self.backgroundColor = backgroundColor
+  }
+}
+
+#Preview("Loading - No Background Color") {
+  MissingArtworkImage(
+    width: 300, artwork: PreviewArtwork(backgroundColor: nil), loadingState: LoadingModel()
+  )
+  .frame(width: 300, height: 300)
+}
+
+#Preview("Loading - Background Color") {
+  MissingArtworkImage(width: 300, artwork: PreviewArtwork(), loadingState: LoadingModel())
+    .frame(width: 300, height: 300)
+}
+
+#Preview("Error") {
+  MissingArtworkImage(
+    width: 300, artwork: PreviewArtwork(), loadingState: LoadingModel(error: CancellationError())
+  )
+  .frame(width: 300, height: 300)
+}
+
+#if canImport(UIKit)
+  private let image = UIImage(systemName: "pencil.circle")
+#else
+  private let image = NSImage(systemSymbolName: "pencil.circle", accessibilityDescription: nil)
+#endif
+
+#Preview("Image") {
+  MissingArtworkImage(
+    width: 300, artwork: PreviewArtwork(),
+    loadingState: LoadingModel(item: PlatformImage(image: image!))
+  )
+  .frame(width: 300, height: 300)
 }
